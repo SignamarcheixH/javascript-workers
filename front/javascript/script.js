86de8c20032ce337c6602a6a8f51adc0b90a0bb0
Hugues Signamarcheix <hugues@vingtcinq.io>
2019-12-11 10:09:39 +0100

    add border on card depending on poke types

--- front/javascript/script.js
+++ front/javascript/script.js
@@ -1,4 +1,24 @@
 const fields = ['name', 'height', 'weight']
+const typesColor = {
+    'normal': '#A8A77A',
+    'fire': '#EE8130',
+    'water': '#6390F0',
+    'electric': '#F7D02C',
+    'grass': '#7AC74C',
+    'ice': '#96D9D6',
+    'fighting': '#C22E28',
+    'poison': '#A33EA1',
+    'ground':  '#E2BF65',
+    'flying': '#A98FF3',
+    'psychic': '#F95587',
+    'bug': '#A6B91A',
+    'rock': '#B6A136',
+    'ghost': '#735797',
+    'dragon': '#6F35FC',
+    'dark': '#705746',
+    'steel': '#B7B7CE',
+    'fairy': '#D685AD',
+}
 
 document.addEventListener("DOMContentLoaded",() => {
     if (window.Worker) {
@@ -6,6 +26,7 @@ document.addEventListener("DOMContentLoaded",() => {
 
         let btnRefresh = document.getElementById("refresh");
         let container = document.getElementsByClassName("container")[0];
+        let card = document.getElementById("text-placeholder");
 
         btnRefresh.addEventListener("click",() => {
             container.classList.add('is-loading');
@@ -18,6 +39,14 @@ document.addEventListener("DOMContentLoaded",() => {
                 document.getElementsByClassName(fieldName)[0].innerHTML = jsonFetched[fieldName];
             }
             document.getElementsByClassName('types')[0].innerHTML = jsonFetched['types'].map(x => x.type.name)
+            if(jsonFetched['types'].length > 1) {
+                card.style.borderTopColor = typesColor[jsonFetched['types'][0].type.name];
+                card.style.borderLeftColor = typesColor[jsonFetched['types'][0].type.name];
+                card.style.borderBottomColor = typesColor[jsonFetched['types'][1].type.name];
+                card.style.borderRightColor = typesColor[jsonFetched['types'][1].type.name];
+            } else {
+                card.style.borderColor = typesColor[jsonFetched['types'][0].type.name];
+            }
             document.getElementsByClassName('sprite')[0].src = jsonFetched['sprites'].front_default;
             container.classList.remove('is-loading');
         }
